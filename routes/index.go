ad9d633cd3e3ca04f491b04f4070b0c42d966082
ombima56 <ombimahillary6@gmail.com>
2024-08-04 11:43:10 +0300

    feat: add error handling

--- routes/index.go
+++ routes/index.go
@@ -1,11 +1,27 @@
 package routes
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-	tmpl.Execute(w,nil)
+	if r.URL.Path != "/" {
+		http.Error(w, "404\nPage Not Found", http.StatusNotFound)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("./templates/index.html")
+	if err != nil {
+		http.Error(w, "404 Page Not Found", http.StatusNotFound)
+		log.Printf("Error parsing templates: %s", err)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Unxpected Error occured try again later", http.StatusInternalServerError)
+		log.Printf("Error occured while executing index.html: %s\n", err)
+		return
+	}
 }
